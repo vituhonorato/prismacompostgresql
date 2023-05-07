fba88fce7a443b29c8345e51f3273bd57f46079f
vituhonorato <vituhonorato.89@gmail.com>
2023-05-07 13:19:24 -0300

    add product-update-releted

---
+++ poc/test-product-update-releted.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+const run = async()=> {
+    const updatedProduct = await prisma.product.update({
+        where:{
+            id:'23e254e0-eeba-4f96-869d-dba7f3c6a6ef'
+        },
+        data:{
+           
+            categories:{
+                connect:[{id:''}]
+            }
+        }
+    });
+    console.log(updatedProduct)
+    
+};
+run();
