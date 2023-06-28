36c36b5c4fc11f2a18d42abbb5f608c19856d354
vituhonorato <vituhonorato.89@gmail.com>
2023-06-28 18:10:33 -0300

    find category

--- poc/test-category-list.ts
+++ poc/test-category-list.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient()
-
+//find category
 const run = async()=> {
     const categories = await prisma.category.findMany()
     console.log(categories)
