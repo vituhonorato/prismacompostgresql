d6810669b9d057bbfdd77c28b7e38e2ba62deab4
vituhonorato <vituhonorato.89@gmail.com>
2023-06-29 19:32:43 -0300

    read mnay category

--- poc/test-connection.ts
+++ poc/test-connection.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient()
-
+//read many categories
 const run = async()=> {
     const categories = await prisma.category.findMany()
     console.log(categories)
