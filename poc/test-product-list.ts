b62d1ad3b7b07769ac7cf0630b006e88413cbb08
vituhonorato <vituhonorato.89@gmail.com>
2023-06-29 19:36:19 -0300

    create product read many

--- poc/test-product-list.ts
+++ poc/test-product-list.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient()
-
+//create product read many
 const run = async()=> {
     const productSaved = await prisma.product.findMany()
     console.log(productSaved)
